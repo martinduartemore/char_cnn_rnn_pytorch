c4282a7a41900741155f057690c5de2ef9059ff9
Martin Duarte <martinduartemore@gmail.com>
2019-08-12 18:09:41 -0300

    Add retrieval evaluation

--- scripts/cub_eval.py
+++ scripts/cub_eval.py
@@ -1,9 +1,10 @@
-python3 sje_eval_classify.py \
+python3 sje_eval.py \
     --seed 123 \
     --dataset birds \
     --model_type icml \
     --data_dir /A/martin/datasets/birds_dataset/cvpr2016_cub \
     --eval_split test \
     --num_txts_eval 0 \
+    --print_class_stats True \
     --batch_size 40 \
     --model_path ckpt/lm_sje_cub_c10_hybrid_0.00070_1_trainval_2019_08_06_20_01_55.pth
--- sje_eval.py
+++ sje_eval.py
@@ -13,14 +13,25 @@ import char_cnn_rnn as ccr
 
 
 
-def eval_classify(net_txt, net_img, data_dir, split, num_txts_eval, batch_size, device):
+def encode_data(net_txt, net_img, data_dir, split, num_txts_eval, batch_size, device):
     path_split_file = os.path.join(data_dir, split+'classes.txt')
     cls_list = [line.rstrip('\n') for line in open(path_split_file)]
 
-    # prepare text data
+    cls_feats_img = []
     cls_feats_txt = []
     for cls in cls_list:
-        print(cls)
+        # prepare image data
+        data_img_path = os.path.join(data_dir, 'images', cls + '.t7')
+        data_img = torch.Tensor(torchfile.load(data_img_path))
+        # cub and flowers datasets have 10 image crops per instance
+        # we use only the first crop per instance
+        feats_img = data_img[:, :, 0].to(device)
+        if net_img is not None:
+            with torch.no_grad():
+                feats_img = net_img(feats_img)
+        cls_feats_img.append(feats_img)
+
+        # prepare text data
         data_txt_path = os.path.join(data_dir, 'text_c10', cls + '.t7')
         data_txt = torch.LongTensor(torchfile.load(data_txt_path))
 
@@ -33,14 +44,15 @@ def eval_classify(net_txt, net_img, data_dir, split, num_txts_eval, batch_size, 
             id_txts = torch.randperm(data_txt.size(0))[:num_txts_eval]
             data_txt = data_txt[id_txts]
 
+        # convert to one-hot tensor to run through network
         # TODO: adapt code to support batched version
         txt_onehot = []
         for txt in data_txt:
             txt_onehot.append(ccr.labelvec_to_onehot(txt))
         txt_onehot = torch.stack(txt_onehot)
 
-        # separate instances into mini-batches
         # if we use a lot of text descriptions, it will not fit in gpu memory
+        # separate instances into mini-batches to process them using gpu
         feats_txt = []
         for batch in torch.split(txt_onehot, batch_size, dim=0):
             with torch.no_grad():
@@ -51,24 +63,19 @@ def eval_classify(net_txt, net_img, data_dir, split, num_txts_eval, batch_size, 
         feats_txt = torch.cat(feats_txt, dim=0).mean(dim=0)
         cls_feats_txt.append(feats_txt)
 
-    cls_feats_txt = torch.stack(cls_feats_txt, dim=0).to(device)
+    cls_feats_txt = torch.stack(cls_feats_txt, dim=0)
 
-    # classification evaluation
+    return cls_feats_img, cls_feats_txt, cls_list
+
+
+
+def eval_classify(cls_feats_img, cls_feats_txt, cls_list):
     cls_stats = OrderedDict()
     for i, cls in enumerate(cls_list):
-        # prepare image data
-        data_img_path = os.path.join(data_dir, 'images', cls + '.t7')
-        data_img = torch.Tensor(torchfile.load(data_img_path))
-        # cub and flowers datasets have 10 image crops per instance
-        # we use only the first crop per instance
-        feats_img = data_img[:, :, 0].to(device)
-        if net_img is not None:
-            with torch.no_grad():
-                feats_img = net_img(feats_img)
-
+        feats_img = cls_feats_img[i]
         scores = torch.matmul(feats_img, cls_feats_txt.t())
-        max_ids = torch.argmax(scores, dim=1)
-        ground_truths = torch.LongTensor(scores.size(0)).fill_(i).to(device)
+        max_ids = torch.argmax(scores, dim=1).to('cpu')
+        ground_truths = torch.LongTensor(scores.size(0)).fill_(i)
         num_correct = (max_ids == ground_truths).sum().item()
         cls_stats[cls] = {'correct': num_correct, 'total': ground_truths.size(0)}
 
@@ -76,6 +83,41 @@ def eval_classify(net_txt, net_img, data_dir, split, num_txts_eval, batch_size, 
     total_correct = sum([stats['correct'] for _, stats in cls_stats.items()])
     avg_acc = total_correct / total
     return avg_acc, cls_stats
+
+
+
+def eval_retrieval(cls_feats_img, cls_feats_txt, cls_list):
+    total_num_cls = cls_feats_txt.size(0)
+    total_num_img = sum([feats.size(0) for feats in cls_feats_img])
+    scores = torch.zeros(total_num_cls, total_num_img)
+    matches = torch.zeros(total_num_cls, total_num_img)
+
+    for i, cls in enumerate(cls_list):
+        start_id = 0
+        for j, feats_img in enumerate(cls_feats_img):
+            end_id = start_id + feats_img.size(0)
+            scores[i, start_id:end_id] = torch.matmul(feats_img, cls_feats_txt[i])
+            if i == j: matches[i, start_id:end_id] = 1
+            start_id = start_id + feats_img.size(0)
+
+    for i, s in enumerate(scores):
+        _, inds = torch.sort(s, descending=True)
+        matches[i] = matches[i, inds]
+
+    k_values = [1, 5, 10, 50]
+
+    map_at_k = OrderedDict()
+    for k in k_values:
+        map_at_k[k] = torch.mean(matches[:, 0:k]).item()
+
+    cls_stats = OrderedDict()
+    for i, cls in enumerate(cls_list):
+        ap_at_k = OrderedDict()
+        for k in k_values:
+            ap_at_k[k] = torch.mean(matches[i, 0:k]).item()
+        cls_stats[cls] = ap_at_k
+
+    return map_at_k, cls_stats
 
 
 
@@ -96,17 +138,42 @@ def main(args):
     net_txt = net_txt.to(device)
     net_txt.eval()
 
-    avg_acc, cls_stats = eval_classify(net_txt, None, args.data_dir, args.eval_split,
-            args.num_txts_eval, args.batch_size, device)
+    cls_feats_img, cls_feats_txt, cls_list = encode_data(net_txt, None, args.data_dir,
+            args.eval_split, args.num_txts_eval, args.batch_size, device)
 
-    for name, stats in cls_stats.items():
-        print('{:.4f}: {}'.format(stats['correct'] / stats['total'], name))
+    mean_ap, cls_stats = eval_retrieval(cls_feats_img, cls_feats_txt, cls_list)
+    print('----- RETRIEVAL -----')
+    if args.print_class_stats:
+        print('  PER CLASS:')
+        for name, stats in cls_stats.items():
+            print(name)
+            for k, ap in stats.items():
+                print('{:.4f}: AP@{}'.format(ap, k))
+        print()
+
+    print('  mAP:')
+    for k, v in mean_ap.items():
+        print('{:.4f}: mAP@{}'.format(v, k))
+    print('---------------------')
+    print()
+
+    avg_acc, cls_stats = eval_classify(cls_feats_img, cls_feats_txt, cls_list)
+    print('--- CLASSIFICATION --')
+    if args.print_class_stats:
+        print('  PER CLASS:')
+        for name, stats in cls_stats.items():
+            print('{:.4f}: {}'.format(stats['correct'] / stats['total'], name))
+        print()
+
     print('Average top-1 accuracy: {:.4f}'.format(avg_acc))
+    print('---------------------')
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, required=True,
+            help='Which RNG seed to use')
     parser.add_argument('--dataset', type=str, required=True,
             choices=['birds', 'flowers'],
             help='Dataset type (birds|flowers)')
@@ -123,24 +190,12 @@ if __name__ == '__main__':
     parser.add_argument('--num_txts_eval', type=int,
             default=0,
             help='Number of texts to use per class (0 = use all available)')
-
+    parser.add_argument('--print_class_stats', type=bool,
+            default=True,
+            help='Whether to print per class statistics or not')
     parser.add_argument('--batch_size', type=int,
             default=40,
             help='Evaluation batch size')
-    parser.add_argument('--image_dim', type=int,
-            default=1024,
-            help='Image feature dimension')
-    parser.add_argument('--doc_length', type=int,
-            default=201,
-            help='Maximum document length')
-    parser.add_argument('--cnn_dim', type=int,
-            default=256,
-            help='Character CNN embedding dimension')
-
-    #parser.add_argument('--gpu_id', type=int, required=True,
-    #        help='Which GPU to use')
-    parser.add_argument('--seed', type=int, required=True,
-            help='Which RNG seed to use')
 
     args = parser.parse_args()
     main(args)
