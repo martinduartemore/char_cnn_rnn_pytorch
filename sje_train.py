d1a719ac0b4f0afe6414632f2f7b561a376d353f
Martin Duarte <martinduartemore@gmail.com>
2019-08-23 15:43:37 -0300

    Fix comments on sje_loss function

--- sje_train.py
+++ sje_train.py
@@ -16,7 +16,7 @@ import char_cnn_rnn as ccr
 
 def sje_loss(feat1, feat2):
     ''' Structured Joint Embedding Loss '''
-    # similarity score matrix (rows: fixed feat1, columns: fixed feat2)
+    # similarity score matrix (rows: fixed feat2, columns: fixed feat1)
     scores = torch.matmul(feat2, feat1.t()) # (B, B)
     # diagonal: matching pairs
     diagonal = scores.diag().view(scores.size(0), 1) # (B, 1)
@@ -25,7 +25,7 @@ def sje_loss(feat1, feat2):
     # calculate costs
     cost = (1 + scores - diagonal).clamp(min=0) # (B, B)
     # clear diagonals (matching pairs are not used in loss computation)
-    #cost[torch.eye(cost.size(0)).bool()] = 0 # (B, B) # for torch==1.2.0
+    #cost[torch.eye(cost.size(0)).bool()] = 0 # (B, B) for torch==1.2.0
     cost[torch.eye(cost.size(0), dtype=torch.uint8)] = 0 # (B, B)
     # sum and average costs
     denom = cost.size(0) * cost.size(1)
